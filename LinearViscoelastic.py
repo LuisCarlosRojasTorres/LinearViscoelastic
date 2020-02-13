81d5cfbe6921fd5c48232ab72ef38e9ed2c9c713
Luis Carlos Absalón Rojas Torres <57602959+LuisCarlosRojasTorres@users.noreply.github.com>
2020-02-13 16:40:37 -0500

    Add files via upload
    Jpp function created, for self heating analysis via finite element method

--- LinearViscoelastic.py
+++ LinearViscoelastic.py
@@ -12,25 +12,24 @@ class LinearViscoelastic:
     '''DOCUMENTATION: A library for a linear viscoelastic solid represented 
     by a n-th order Prony Serie'''
 
-    def __init__(self,stress,rTimes,Tref=23,c1=7.32,c2=93.62):
-        self.stress = np.array(stress)     #Array of Elastic Modules
+    def __init__(self,stress,rTimes,v=0.45,Tref=23,c1=7.32,c2=93.62):
+        self.stress = np.array(stress)   #Array of Elastic Modules
         self.rTimesTref = np.array(rTimes) ##Array of Relaxation Times at Tref
         self.rTimes = np.array(rTimes)     #Array of Relaxation Times at T
         self.T = Tref
         self.Tref = Tref              #Reference temperature
         self.c1 = c1                  #WLF coefficient c1
         self.c2 = c2                  #WLF coefficient c2
-    
+        self.v = v                    #Poisson's Ratio 
+
     def modify_T(self,T):
         #Change the temperature of analysis
 	    self.T=T                    #Given temperature
 	    self.rTimes=self.rTimesTref*self.get_shiftFactor()
 	
-
     def get_shiftFactor(self):
         #Calculates the shift factor WLF at a given temperature (T)
     	return 10**(-self.c1*(self.T-self.Tref)/(self.c2+self.T-self.Tref))
-
 
     def get_Er(self,T,time=0):
         Er=self.stress[0]
@@ -56,3 +55,9 @@ class LinearViscoelastic:
         for i in range(1,len(self.stress)):
             Epp+=(self.stress[i]*w*self.rTimes[i-1])/(1+(w*self.rTimes[i-1])**2)
         return Epp
+    
+    def get_Jpp(self,T,frec):
+        Jpp=((1+self.v)*self.get_Epp(T,frec))
+        Jpp/=(self.get_Ep(T,frec)**2+self.get_Epp(T,frec)**2)
+        return Jpp
+        
